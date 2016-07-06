45d5c43eb8b35c497ac6f44e66c6f90b1b489294
Nagibator3000 <fetakonfeta@gmail.com>
2016-07-06 17:17:00 +0300

    -add picasso

--- app/src/main/java/com/kracubo/pictureaplication/InFileActivity.java
+++ app/src/main/java/com/kracubo/pictureaplication/InFileActivity.java
@@ -2,12 +2,16 @@ package com.kracubo.pictureaplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InFileActivity extends AppCompatActivity {
-
+     Button openFileDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_file);
+
+
     }
 }
--- app/src/main/java/com/kracubo/pictureaplication/InLinkActivity.java
+++ app/src/main/java/com/kracubo/pictureaplication/InLinkActivity.java
@@ -1,13 +1,44 @@
 package com.kracubo.pictureaplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class InLinkActivity extends AppCompatActivity {
-
+    ImageView imImageView;
+    Button buttonLinkOk;
+    EditText editTextLink;
+    String urlTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_link);
+        buttonLinkOk = (Button) findViewById(R.id.btnOkLink);
+        editTextLink = (EditText) findViewById(R.id.editTxtLink);
+        imImageView = (ImageView) findViewById(R.id.imgViewLink);
+
+
+
+
+        buttonLinkOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+          //      urlTitle = String.valueOf(editTextLink.getText());
+                Picasso.with(InLinkActivity.this)
+                        .load("http://zastavki-oboi.ru/thumbs/melkie_lepestki-4620.jpg")
+                        .placeholder(R.drawable.ic_swap_vert_black_24dp)
+                        .error(R.drawable.ic_highlight_off_black_24dp)
+                        .resize(300,300)
+                        .into(imImageView);
+
+            }
+        });
     }
 }
